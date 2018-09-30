3a082277217e2c4684dfb760d98891c16da926aa
streamlesswaveson <streamlesswaveson@gmail.com>
2018-09-30 09:06:16 -0700

    pojos

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Person {
+    private String firstName;
+    private String lastName;
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinic.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PetType {
+    private String name;
+}
