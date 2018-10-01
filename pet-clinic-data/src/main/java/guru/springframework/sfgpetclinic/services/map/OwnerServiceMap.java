16c3a7f1e7b6b28419c9e3490dff04607dcb8ecc
streamlesswaveson <streamlesswaveson@gmail.com>
2018-09-30 19:59:17 -0700

    5-87 - dataloader/command line runner

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -1,12 +1,17 @@
 package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Owner;
-import guru.springframework.sfgpetclinic.services.CrudService;
-import guru.springframework.sfgpetclinic.services.map.AbstractMapService;
+import guru.springframework.sfgpetclinic.services.OwnerService;
 
-public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements OwnerService {
     @Override
     public Owner save(Owner obj) {
         return super.save(obj, obj.getId());
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        //FIXME
+        return null;
     }
 }
---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -0,0 +1,58 @@
+package guru.springframework.sfgpetclinic.bootstrap;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.VetService;
+import guru.springframework.sfgpetclinic.services.map.OwnerServiceMap;
+import guru.springframework.sfgpetclinic.services.map.VetServiceMap;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner{
+    private OwnerService ownerService;
+    private VetService vetService;
+
+    public DataLoader() {
+        this.vetService = new VetServiceMap();
+        this.ownerService = new OwnerServiceMap();
+    }
+
+    // the run method will run at startup
+    @Override
+    public void run(String... args) throws Exception {
+        Owner owner1 = new Owner();
+        owner1.setFirstName("joe");
+        owner1.setLastName("blow");
+        owner1.setId(1l);
+
+        ownerService.save(owner1);
+
+        Owner owner2 = new Owner();
+        owner2.setFirstName("shelly");
+        owner2.setLastName("duval");
+        owner2.setId(2l);
+
+        ownerService.save(owner2);
+
+        System.out.println("loaded owners");
+
+        Vet vet1 = new Vet();
+        vet1.setFirstName("hermione");
+        vet1.setLastName("granger");
+        vet1.setId(3l);
+
+        vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setFirstName("hermione");
+        vet2.setLastName("granger");
+        vet2.setId(4l);
+
+        vetService.save(vet2);
+
+        System.out.println("loaded vets");
+
+    }
+}
