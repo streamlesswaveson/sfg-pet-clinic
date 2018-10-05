7f1ef190237409907f19b66158a0d2ea6d676cb4
streamlesswaveson <streamlesswaveson@gmail.com>
2018-10-05 04:53:42 -0700

    7-130 - more boostrap code

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -2,10 +2,20 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements OwnerService {
+
+    private final PetService petService;
+    private final PetTypeService petTypeService;
+
+    public OwnerServiceMap(PetService petService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.petTypeService = petTypeService;
+    }
 
     @Override
     public Owner findByLastName(String lastName) {
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,36 +1,55 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
 public class DataLoader implements CommandLineRunner{
-    private OwnerService ownerService;
-    private VetService vetService;
+    private final OwnerService ownerService;
+    private final VetService vetService;
+    private final PetTypeService petTypeService;
+    private final PetService petService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, PetService petService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
+        this.petService = petService;
     }
 
     // the run method will run at startup
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = makePetType("dog");
+        PetType cat = makePetType("cat");
+
         Owner owner1 = new Owner();
         owner1.setFirstName("joe");
         owner1.setLastName("blow");
+        owner1.setAddress("123 Foo St");
+        owner1.setCity("Miami");
+        owner1.setTelephone("818-123-3456");
 
-        ownerService.save(owner1);
+        owner1 = ownerService.save(owner1);
+        Pet pet1 = makePet("stan", LocalDate.of(2015,1,20), dog, owner1);
+
 
         Owner owner2 = new Owner();
         owner2.setFirstName("shelly");
         owner2.setLastName("duval");
 
-        ownerService.save(owner2);
+        owner2 = ownerService.save(owner2);
+        Pet pet2 = makePet("stupid cat", LocalDate.now(), cat, owner2);
 
         System.out.println("loaded owners");
 
@@ -48,5 +67,23 @@ public class DataLoader implements CommandLineRunner{
 
         System.out.println("loaded vets");
 
+    }
+
+    private Pet makePet(String name, LocalDate date, PetType type, Owner owner) {
+        Pet pet = new Pet();
+        pet.setBirthDate(date);
+        pet.setPetType(type);
+        pet.setName(name);
+        pet.setOwner(owner);
+        owner.getPets().add(pet);
+
+        return petService.save(pet);
+
+    }
+
+    private PetType makePetType(String type) {
+        PetType t = new PetType();
+        t.setName(type);
+        return petTypeService.save(t);
     }
 }
