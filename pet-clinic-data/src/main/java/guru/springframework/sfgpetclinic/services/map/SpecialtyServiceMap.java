aa20823afe4bc9985a58756bb9fda441fd69ef65
streamlesswaveson <streamlesswaveson@gmail.com>
2018-10-05 04:57:11 -0700

    7-131 - specialty service

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+import guru.springframework.sfgpetclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty,Long> implements SpecialtyService{
+}
