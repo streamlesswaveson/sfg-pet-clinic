476708db7d559b2947633ba49926cef6a4c2be96
streamlesswaveson <streamlesswaveson@gmail.com>
2018-10-08 04:47:52 -0700

    8-160 - pet service

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSpringDataJPAService.java
@@ -0,0 +1,47 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.repository.PetRepository;
+import guru.springframework.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSpringDataJPAService implements PetService {
+    private final PetRepository petRepository;
+
+    public PetSpringDataJPAService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet save(Pet obj) {
+        return petRepository.save(obj);
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public void delete(Pet obj) {
+        petRepository.delete(obj);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
